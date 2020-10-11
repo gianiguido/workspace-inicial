5feee3522dbcd7bcb5a5198c32c03b45e4d68eb6
gianiguido <gianiguido@gmail.com>
2020-10-11 20:19:41 -0300

    Entrega 5
    Modificaciones en Cart.html y cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,56 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+var cartInfo = [];
+var cartArticles = [];
+var shippingPercentage = 0.15;
+let DOLLAR_SYMBOL = "USD ";
+let PESO_SYMBOL = "UYU ";
+
+
 document.addEventListener("DOMContentLoaded", function(e){
 
+    getJSONData(CART_URL).then(function(resultObj) {
+        let htmlContentToAppend = "";
+        if (resultObj.status === "ok") {
+            cartInfo = resultObj.data;
+            cartArticles = cartInfo.articles;
+            for (var i = 0; i < cartInfo.articles.length; i++) {
+                htmlContentToAppend += `
+                <tr>
+                    <th scope="row"><img src="`+ cartArticles[i].src +`" alt="" width="120px"></th>
+                    <td>`+ cartArticles[i].name +`</td>
+                    <td>`+ cartArticles[i].currency + ` `  + cartArticles[i].unitCost +`</td>
+                    <td><input class="form-control" style="width: 150px;" type="number" id="contadorProd">`+ cartArticles[i].count +`</td>
+                    <td class="font-weight-bold" id="productSubtotal">UYU 100</td>
+                </tr>`
+              document.getElementById("tablaCarrito").innerHTML = htmlContentToAppend;
+            }
+        } 
+    });
+
+ 
+      
+    
+    document.getElementById("contadorProd").addEventListener("change",function(){
+        var contadorArt = document.getElementById("contadorProd").value;
+        var resultado = contadorArt * unitCost;
+    });
+
+    document.getElementById("premiumRadio").addEventListener("change", function(){
+        shippingPercentage = 0.15;
+        costoTotal();
+    });
+    
+    document.getElementById("expressRadio").addEventListener("change", function(){
+        shippingPercentage = 0.07;
+        costoTotal();
+    });
+
+    document.getElementById("standardRadio").addEventListener("change", function(){
+        shippingPercentage = 0.05;
+        costoTotal();
+    });
 });
+
+
+   
+    
+   
